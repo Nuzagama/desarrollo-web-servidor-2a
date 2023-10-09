b073bf2e0896348c85c1d9ba5ae29eb58b92d79b
Nuzagama <alvarorc25@gmail.com>
2023-10-09 23:32:02 +0200

    Ejercicio Terminado

--- formularios/muchos_formularios.php
+++ formularios/muchos_formularios.php
@@ -13,6 +13,7 @@
     <!-- Formulario Temperatura -->
     <form action = "" method="post">
         <fieldset>
+            <legend>Convertidor de Temperatura</legend>
         <br>
         <label>Convertidor de Temperatura</label>
         <input type = "text" name= "temperaturaOriginal">
@@ -52,6 +53,7 @@
         <!-- Formulario Max3 -->
         <form action = "" method="post">
         <fieldset>
+            <legend>Comparador de Máximos</legend>
         <br>
         <label>Comparador de Máximos o 3 iguales</label>
         <input type = "text" name= "primerNum">
@@ -77,6 +79,65 @@
         ?>
         </fieldset>
     </form>
+
+    <br><br><br>
+
+<!-- Formulario Potencia -->
+<form action = "" method="post">
+<fieldset>
+    <legend>Calculadora de Potencia</legend>
+<br>
+<label>Hacer Potencia de un número</label>
+<input type = "number" name= "base">
+<input type = "number" name= "potencia">
+<input type = "hidden" name="action" value="potencia">
+<br>
+<input type="submit" value="Calcular">
+
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+if($_POST["action"] == "potencia"){
+$num = (int)$_POST["base"];
+$elevado = (int)$_POST["potencia"];
+$total = calcularPotencia($num,$elevado);
+
+echo "<br><br><b>Resultado</b><br>";
+echo $total;
+}
+}
+?>
+</fieldset>
+</form>
+
+<br><br><br>
+
+<!-- Formulario Primos -->
+<form action = "" method="post">
+<fieldset>
+    <legend>Calculadora de Primos</legend>
+<br>
+<label>Número límite para calcular primos</label>
+<input type = "number" name= "numLimite">
+<input type = "hidden" name="action" value="primo2">
+<br>
+<input type="submit" value="Calcular">
+
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_POST["action"] == "primo2") {
+        $numeroLimite = (int)$_POST["numLimite"];
+        $arrayPrimos = mostrarPrimos2($numeroLimite);
+
+        echo "<br><br><b>Resultado</b><br>";
+        echo implode(', ', $arrayPrimos); // Convierte el array en una cadena separada por comas
+    }
+}
+
+?>
+</fieldset>
+</form>
     
 </body>
 </html>
